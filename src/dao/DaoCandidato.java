7f002bab62b5a244d1f065d5f91bead2327f02e5
Julio César Paredes <juliocepar@hotmail.com>
2016-10-12 01:39:13 -0400

    Validación VRegistroCandidato, nueva clase, intento de conexion con BD
    Se implementó la validación de la vista de Registrar Candidato (llamada 
    ahora en el encabezado, mas no en el nombre del archivo, "Gestionar
    candidato").
    Se añadió la clase Validaciones del otro proyecto que nos 
    pasaron al paquete "modelo", y se implementaron algunos métodos de la
    clase recién creada DaoCandidato (todavía no funciona).

---
+++ src/dao/DaoCandidato.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import Modelo.Validaciones;
+import conexion.ConexionSqlite1;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import modelo.MCandidato;
+/**
+ *
+ * @author Julio César
+ */
+
+public class DaoCandidato {
+    private ConexionSqlite1 con;
+
+    public DaoCandidato() {
+        con = new ConexionSqlite1();
+    }
+    
+    public void InsertarCandidato(MCandidato can) {
+        String sql = "INSERT INTO TCandidato (CanRif, CanNombres, CanApellidos, CanFechaNac, CanEdoCivil, CanDireccion, CanPais, CanCiudad, CanEmail, CanTelefono, CanEstatus) VALUES(";
+        sql += Validaciones.Apost(can.getRif());
+        sql += Validaciones.Apost(can.getNombres()) + ", ";
+        sql += Validaciones.Apost(can.getApellidos()) + ", ";
+        sql += new SimpleDateFormat("yyyy-MM-dd").format(can.getFechaNacimiento()) + ", ";
+        sql += Validaciones.Apost(String.valueOf(can.getEdoCivil())) + ", ";
+        sql += Validaciones.Apost(can.getDireccion()) + ", ";
+        sql += Validaciones.Apost(can.getPais()) + ", ";
+        sql += Validaciones.Apost(can.getCiudad()) + ", ";
+        sql += Validaciones.Apost(can.getEmail()) + ", ";
+        sql += Validaciones.Apost(can.getTelefono()) + ", ";
+        sql += "'A')";
+        con.ejecutar(sql);
+    }
+    
+    public ResultSet BuscarCandidato(String id) {
+        ResultSet res;
+        con = new ConexionSqlite1();
+        String sql = "SELECT * FROM TCandidato WHERE CanRif='" + id + "'";
+        res = con.consultar(sql);
+        return res;
+    }
+}
+
